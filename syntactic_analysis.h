71fae8d9cdeba88e802774b29481b544634d8a83
ami-GS <1991.daiki@gmail.com>
2016-07-27 13:31:32 +0000

    add function difinitions

--- syntactic_analysis.h
+++ syntactic_analysis.h
@@ -6,5 +6,17 @@ int stack[STACK_SIZ];
 int stack_c = 0;
 int variables[26]; // TODO : future, hash table?
 
+void expression(FILE *f, Token *t);
+void or_exp(FILE *f, Token *t);
+void and_exp(FILE *f, Token *t);
+void equ_exp(FILE *f, Token *t);
+void rel_exp(FILE *f, Token *t);
+void add_sub_exp(FILE *f, Token *t);
+void mul_div_mod_exp(FILE *f, Token *t);
+void factor(FILE *f, Token *t);
+void operate(Kind op);
+void push(int n);
+int pop();
+
 
 #endif // DCC_SYNTACTIC_ANALYSIS_H_
