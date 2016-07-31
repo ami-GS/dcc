8eb4cc4e7adf19c87b850df1b746d1e8444651e2
ami-GS <1991.daiki@gmail.com>
2016-07-31 12:13:07 +0900

    implement partially

--- parse.c
+++ parse.c
@@ -13,12 +13,68 @@ void compile(char *fname) {
     switch(t.kind) {
       // type, #, ;, }, )
     case Int: case Void:
-      set_type(&entryTmp);
-      set_name(&entryTmp);
+      set_type(&entryTmp, &t);
+      set_name(&entryTmp, &t);
+      if (t->kind == Lparen) {
+	declare_func();
+      } else { // in case of ','
+	declare_var(&entryTmp, &t);
+      }
     case Semicolon:
       nextToken(&t);
     }
   }
+}
 
-  
+
+void set_dtype(TableEntry* ent, Token* t) {
+  ent->name""
+  switch(t->kind) {
+  case Int:
+    ent->dType = INT_T;
+    break;
+  case Void:
+    ent->dType = VOID_T;
+    break;
+  default:
+    break;
+    // TODO : other types can be placed
+  }
+  nextToken(&t);
+  return;
+}
+
+int set_name(TableEntry* ent, Token* t) {
+  if (k != Ident) {
+    // TODO : error
+    return -1;
+  }
+  int len;
+  for (len = 0; t->text+len != '\0'; len++) {}
+  ent->name = malloc(sizeof(char) * (len + 1)); // TODO : error check, and must free
+  for (len = 0; t->text+len != '\0'; len) {
+    *(ent->name+len) = t->text+len;
+  }
+  ent->name+len = '\0';
+  nextToken(t); // there are cases of ',', '[', '(', ';'
+  return 1;
+}
+
+int set_array() {
+  return 1;
+}
+
+int declare_var(TableEntry* ent, Token* t) {
+  while (1) {
+    if (t->kind == Lbracket) {
+      set_array(ent);
+    }
+    enter_table_item(ent);
+    if (t->kind == Semicolon || t->kind != Comma) {
+      break; // TODO : suspicious
+    }
+    nextToken(t); // next to ','
+    set_name(ent, t);
+  }
+  return checkNxtTokenKind(Semicolon);
 }
--- parse.h
+++ parse.h
@@ -2,5 +2,11 @@
 #define _DCC_PARSE_H_
 
 void compile(char *fname);
+void set_dtype(TableEntry* ent, Token* t);
+int set_name(TableEntry* ent, Token* t);
+int set_array();
+int declare_func();
+int declare_var(TableEntry* ent, Token* t);
+
 
 #endif // _DCC_PARSE_H_
