9b13e4566046bf0c76020e548eb83d7fa50531b5
ami-GS <1991.daiki@gmail.com>
2016-10-25 23:10:37 +0900

    implement convenient array

--- data_declare.h
+++ data_declare.h
@@ -2,8 +2,14 @@
 #define _DCC_DATA_DECLARE_H_
 
 #define INT_SIZE 4
+#define FLOAT_SIZE 4
+#define SHORT_SIZE 2
 #define DOUBLE_SIZE 8
 #define CHAR_SIZE 1
 #define POINTER_SIZE INT_SIZE // TODO 8?. currently, this should be same as INT_SIZE
+
+static int DATA_SIZE[] = {-1, -1, POINTER_SIZE, INT_SIZE, POINTER_SIZE,
+                          SHORT_SIZE, POINTER_SIZE, CHAR_SIZE, POINTER_SIZE,
+                          FLOAT_SIZE, POINTER_SIZE, DOUBLE_SIZE, POINTER_SIZE};
 
 #endif //_DCC_DATA_DECLARE_H_
