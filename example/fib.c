ac54ae8f9426bb98a38dfe063ac18943e90760b3
ami-GS <1991.daiki@gmail.com>
2016-12-09 23:26:41 +0900

    add memo recursive

--- example/fib.c
+++ example/fib.c
@@ -7,8 +7,10 @@ Usage
 >> dcc example/fig.c -srm
 
 Final output line should be like
-1: STOP 0 34
+1: STOP 0 0
 */
+
+int memo[126];
 
 int fib_recursive(int c) {
   // recursive implementation (slow)
@@ -32,8 +34,20 @@ int fib_loop(int c) {
   return bef;
 }
 
+int fib_memo(int c) {
+  if (c == 0)
+    return 0;
+  else if (c == 1)
+    return 1;
+  else if (memo[c] != 0)
+    return memo[c];
+  else
+    memo[c] = fib_memo(c-2) + fib_memo(c-1);
+  return memo[c];
+}
+
 int main() {
   int level = 11;
-  return fib_loop(level) - fib_recursive(level-1);
-  // 89 - 55 = 34
+  return fib_memo(level+1) - fib_loop(level) - fib_recursive(level-1);
+  // 144 - 89 - 55 = 0
 }
